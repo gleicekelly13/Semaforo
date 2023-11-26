83ca2f66c428b97ed6c8a633ab9aff72f9cb320f
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-11-26 13:51:30 -0300

    Alteração nas funcionalidades e ativação do modo automático

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ var luzVermelha = document.getElementById("light-red");
 var luzAmarela = document.getElementById("light-yellow");
 var luzVerde = document.getElementById("light-green")
 
+let automaticInterval;
+let automaticMode = false;
+
 var btnRed = document.getElementById("buttonRed");
 var btnYellow = document.getElementById("buttonYellow");
 var btnGreen = document.getElementById("buttonGreen");
+var btnAuto = document.getElementById("buttonAuto");
 
-btnRed.addEventListener("click", function acenderLuz() {
+/*btnRed.addEventListener("click", function acenderLuz() {
     luzVermelha.classList.toggle("red");
     luzAmarela.classList.remove("yellow");
     luzVerde.classList.remove("green");
@@ -22,8 +26,73 @@ btnGreen.addEventListener("click", function acenderLuz() {
     luzVermelha.classList.remove("red");
     luzAmarela.classList.remove("yellow");
     luzVerde.classList.toggle("green");
+}) */
+
+function acenderLuz(red, yellow, green) {
+    luzVermelha.classList.toggle("red", red);
+    luzAmarela.classList.toggle("yellow", yellow);
+    luzVerde.classList.toggle("green", green);
+}
+
+function acenderLuzManual(red, yellow, green) {
+    acenderLuz(red, yellow, green);
+    automaticMode = false;
+}
+
+function modoAutomatico() {
+    if (automaticMode) {
+        acenderLuz(true, false, false);
+        setTimeout(function() {
+            acenderLuz(false, true, false);
+        }, 1000);
+        setTimeout(function() {
+            acenderLuz(false, false, true);
+        }, 2000); 
+    }
+}
+
+btnRed.addEventListener("click", function() {
+    acenderLuzManual(true, false, false);
+});
+
+btnYellow.addEventListener("click", function() {
+    acenderLuzManual(false, true, false);
+});
+
+btnGreen.addEventListener("click", function() {
+    acenderLuz(false, false, true);
 })
 
+btnAuto.addEventListener("click", function() {
+    automaticMode = !automaticMode;
+
+    if (automaticMode) {
+        automaticInterval = setInterval(modoAutomatico, 3000);
+        modoAutomatico();
+    } else {
+        clearInterval(automaticInterval);
+    }
+})
+
+
+/*btnAuto.addEventListener("click", function() {
+    automaticMode = !automaticMode;
+
+    if(automaticMode) {
+        automaticInterval = setInterval(function() {
+            acenderLuz(true, false, false);
+            setTimeout(function() {
+                acenderLuz(false, true, false);
+            }, 1000);
+            setTimeout(function() {
+                acenderLuz(false, false,true);
+            }, 2000);
+        }, 3000);
+    } else {
+        clearInterval(automaticInterval);
+    }
+})
+*/
 
 /*
 * classList => Manipula as classes
